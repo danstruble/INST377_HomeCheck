1f732e046caf13325e865cb3f4bb57e9cfcdef1c
jonathanchen985 <60477056+jonathanchen985@users.noreply.github.com>
2020-05-06 23:51:59 -0400

    Update DocumentationContent.js

--- public/components/DocumentationContent.js
+++ public/components/DocumentationContent.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 
-
-
 const DocumentationContent = () => (
     <div className = "container">
         <div className = "wrapper">
@@ -11,7 +9,7 @@ const DocumentationContent = () => (
                 <h1>How to Use Our App </h1>
             </header>
             
-                <h2>React JS</h2>
+                <h2>Next JS</h2>
                 <p>We used React JS in order to create an interactive interface.</p>
                 <p>Most of the content seen on our website was integrated by using Javascript. </p>
                 <p>We create various components that were exported to the front end of our various pages.</p>
@@ -24,6 +22,7 @@ const DocumentationContent = () => (
                  <h3>API</h3>
                  <p>In order to access the PG county data, we used a fetch api call to get the content on the various housing inspection violations.</p>
                  <p>This data was stored into a server on the backend. Afterwards users on the site would be able to access that data through a fetch api call.</p>
+
                  
                  
                    
