0d7105ae3be305fe64813e632bd573bb13cca1bf
kngo1337 <60367871+kngo1337@users.noreply.github.com>
2020-05-07 00:39:24 -0400

    hope this works

--- public/.next/static/runtime/webpack.js
+++ public/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8d7182051b5d9729a105";
+/******/ 	var hotCurrentHash = "82490819d12a68fe6b30";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
