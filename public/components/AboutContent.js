9b727786a3b335b79988593f1048f8b9e5ec6be3
xiuweili1999 <60372954+xiuweili1999@users.noreply.github.com>
2020-05-07 10:59:20 -0400

    updated styles and added image

--- public/components/AboutContent.js
+++ public/components/AboutContent.js
@@ -4,130 +4,122 @@ const AboutContent = () => (
   <div className="container">
     <div className="wrapper">
       <h1>We are HomeCheck</h1>
-      <div className="content">
+      <div className="description">
         <p>
           Building code violations are problematic to prospective buyers and
           renters, as they can be costly to fix, and can pose a health/safety
           risk. We recognize the need for buyers and renters to know the
           building violations before purchase or rent; however, it's often
-          difficult for them to find this information. For Prince George's
-          County, although there is a database with all house inspection
-          violations, it is not easily accessible to those who are trying to
-          find information on a certain area or home. Hence, we propose
-          HomeCheck as a solution to solve this information issue. HomeCheck
-          compiles Prince George's County House Inspections data and creates a
-          visual tool with a search feature to find violations. Our team hopes
-          to provide a user-friendly web application that helps Prince George's
-          buyers and renters to have easy access to building code violations. We
-          strongly encourage buyers and renters of Prince George's County to use
-          our application before deciding on any home property.
+          difficult for them to find this information.
+          <pre></pre>
+          For Prince George's County, although there is a database with all
+          house inspection violations, it is not easily accessible to those who
+          are trying to find information on a certain area or home. Hence, we
+          propose HomeCheck as a solution to solve this information issue.
+          <pre></pre>
+          HomeCheck compiles Prince George's County House Inspections data and
+          creates a visual tool with a search feature to find violations. Our
+          team hopes to provide a user-friendly web application that helps
+          Prince George's buyers and renters to have easy access to building
+          code violations. We strongly encourage buyers and renters of Prince
+          George's County to use our application before deciding on any home
+          property.
         </p>
       </div>
       <h1>Meet the Team</h1>
-      <div className="content">
-        <div className="line-two">
-          <div className="one">
-            <div className="img1">
-              <img src="../static/images/img1.png" alt="img1"></img>
-            </div>
-            <div className="person1">
-              <div className="Name1">
-                <p>Jonathan Chen</p>
-              </div>
-              <div className="Intro1">
-                <p>
-                  My role in the project was in developing the front end and
-                  writing documentation.
-                </p>
-              </div>
-              <div className="interests1">
-                <p>
-                  My interests include playing basketball, listening to music
-                  and playing the piano. I also enjoy learning about data
-                  science techniques.
-                </p>
-              </div>
-            </div>
+      <div className="about_team">
+        <div className="team_member">
+          <div className="img">
+            <img src="../static/images/img1.jpeg" alt="img1"></img>
           </div>
-          <div className="two">
-            <div className="img2">
-              <img src="../static/images/img2.png" alt="img2"></img>
+          <div className="person">
+            <div className="name">
+              <b>Jonathan Chen</b>
             </div>
-            <div className="person2">
-              <div className="Name2">
-                <p>Xiuwei Li</p>
-              </div>
-              <div className="Intro2">
-                <p>
-                  My role in the project was in the development of the
-                  wireframes and documentation, and writing the presentations.
-                </p>
-              </div>
-              <div className="interests2">
-                <p>
-                  My interests include enjoying the company of cats, playing
-                  games, and cooking.
-                </p>
-              </div>
+            <div className="role">
+              <p>Front-end Developer & Documentation Writer</p>
             </div>
-          </div>
-          <div className="three">
-            <div className="img3">
-              <img src="../static/images/img3.png" alt="img3"></img>
-            </div>
-            <div className="person3">
-              <div className="Name3">
-                <p>Kevin Ngo</p>
-              </div>
-              <div className="Intro3">
-                <p>
-                  My role in the project was in front end development and
-                  helping with creation of presentations.
-                </p>
-              </div>
-              <div className="interests3">
-                <p>
-                  My interests include taking over the world, and killing the
-                  Joestar bloodline.
-                </p>
-              </div>
+            <div class="divider"></div>
+            <div className="interests">
+              <p>
+                My interests include playing basketball, listening to music and
+                playing the piano. I also enjoy learning about data science
+                techniques.
+              </p>
             </div>
           </div>
         </div>
-        <div className="line-three">
-          <div className="four">
-            <div className="img4">
-              <img src="../static/images/img4.png" alt="img4"></img>
+        <div className="team_member">
+          <div className="img">
+            <img src="../static/images/img2.jpg" alt="img2"></img>
+          </div>
+          <div className="person">
+            <div className="name">
+              <b>Xiuwei Li</b>
             </div>
-            <div className="person4">
-              <div className="Name4">
-                <p>Dalton Stoner</p>
-              </div>
-              <div className="Intro4">
-                <p>
-                  My role in the project was in front end development, and
-                  helping to write the presentations.
-                </p>
-              </div>
-              <div className="interests4">
-                <p>I am interested in long walks on the beach, and cooking.</p>
-              </div>
+            <div className="role">
+              <p>CSS Stylist & Wireframe Creator & Documentation Writer</p>
+            </div>
+            <div class="divider"></div>
+            <div className="interests">
+              <p>
+                My interests include enjoying the company of cats, playing
+                games, and cooking.
+              </p>
             </div>
           </div>
-          <div className="five">
-            <div className="img5">
-              <img src="../static/images/img5.png" alt="img5"></img>
+        </div>
+        <div className="team_member">
+          <div className="img">
+            <img src="../static/images/img3.png" alt="img3"></img>
+          </div>
+          <div className="person">
+            <div className="name">
+              <b>Kevin Ngo</b>
             </div>
-            <div className="person5">
-              <div className="Name5">
-                <p>Dan Struble</p>
-              </div>
-              <div className="Intro5">
-                <p>My role in the project was doing backend development.</p>
-              </div>
-              <div className="interests5">
-                <p>My interests are in networking and visualization.</p>
-              </div>
+            <div className="role">
+              <p>Front-end Developer & Presentation Writer</p>
+            </div>
+            <div class="divider"></div>
+            <div className="interests">
+              <p>
+                My interests include taking over the world, and killing the
+                Joestar bloodline.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="team_member">
+          <div className="img">
+            <img src="../static/images/img4.jpg" alt="img4"></img>
+          </div>
+          <div className="person">
+            <div className="name">
+              <b>Dalton Stoner</b>
+            </div>
+            <div className="role">
+              <p>Front-end Developer & Presentation Writer</p>
+            </div>
+            <div class="divider"></div>
+            <div className="interests">
+              <p>I am interested in long walks on the beach, and cooking.</p>
+            </div>
+          </div>
+        </div>
+        <div className="team_member">
+          <div className="img">
+            <img src="../static/images/img5.jpg" alt="img5"></img>
+          </div>
+          <div className="person">
+            <div className="name">
+              <b>Dan Struble</b>
+            </div>
+            <div className="role">
+              <p>Back-end Developer & API Endpoints Tester</p>
+            </div>
+            <div class="divider"></div>
+            <div className="interests">
+              <p>My interests are in networking and visualization.</p>
             </div>
           </div>
         </div>
--- public/components/ContactForm.js
+++ public/components/ContactForm.js
@@ -1,29 +1,51 @@
-import React from 'react';
+import React from "react";
 
 const ContactForm = () => (
   <div className="container">
-    <div className = "wrapper">
-        <h1>Contact Us</h1>
+    <div className="wrapper">
+      <div class="section_one">
+        <div className="email_image">
+          <img src="../static/images/email.png" alt="email"></img>
+        </div>
+      </div>
+      <div class="section_two">
+      <h1>Contact Us</h1>
         <p>Please reach out to us if you have any questions!</p>
-      <article className = "contactMain">
-        <form>
-          <ul>
-            <li>
-              <input type="text" id="name" name="user_name" placeholder="Name" maxLength ="140" />
-            </li>
-            <li>
-              <input type = "text" id="email" name="user_email" placeholder = "Email" />
-            </li>
-            <li>
-              <input type = "text" id="message" name="user_message" placeholder = "Message" />
-            </li>
-          </ul>
-        </form>
-        <button type="submit">Send</button>
-      </article>
+        <article className="contactMain">
+          <form>
+            <ul>
+              <li>
+                <input
+                  type="text"
+                  id="name"
+                  name="user_name"
+                  placeholder="Name"
+                  maxLength="140"
+                />
+              </li>
+              <li>
+                <input
+                  type="text"
+                  id="email"
+                  name="user_email"
+                  placeholder="Email"
+                />
+              </li>
+              <li>
+                <input
+                  type="text"
+                  id="message"
+                  name="user_message"
+                  placeholder="Message"
+                />
+              </li>
+            </ul>
+          </form>
+          <button type="submit">Send</button>
+        </article>
+      </div>
     </div>
   </div>
-
 );
 
 export default ContactForm;
