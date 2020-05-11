392da485510225bc789cc5c6a7d16fdcc19c4eb2
xiuweili1999 <60372954+xiuweili1999@users.noreply.github.com>
2020-05-10 21:38:28 -0400

    updated contact us page

--- public/components/ContactForm.js
+++ public/components/ContactForm.js
@@ -4,45 +4,54 @@ const ContactForm = () => (
   <div className="container">
     <div className="wrapper">
       <div class="section_one">
-        <div className="email_image">
-          <img src="../static/images/email.png" alt="email"></img>
-        </div>
+        <h1>Don't be a stranger</h1>
+        <h1>just reach out.</h1>
       </div>
-      <div class="section_two">
-      <h1>Contact Us</h1>
-        <p>Please reach out to us if you have any questions!</p>
-        <article className="contactMain">
-          <form>
-            <ul>
-              <li>
-                <input
-                  type="text"
-                  id="name"
-                  name="user_name"
-                  placeholder="Name"
-                  maxLength="140"
-                />
-              </li>
-              <li>
-                <input
-                  type="text"
-                  id="email"
-                  name="user_email"
-                  placeholder="Email"
-                />
-              </li>
-              <li>
-                <input
-                  type="text"
-                  id="message"
-                  name="user_message"
-                  placeholder="Message"
-                />
-              </li>
-            </ul>
-          </form>
-          <button type="submit">Send</button>
-        </article>
+      <div className="contact_wrapper">
+        <div className="section_two">
+          <p>
+            Feel free to get in touch with us with any questions or inquiries.
+            We are always open to discussing new projects, creative ideas or
+            opportunities to be part of your visions.
+          </p>
+          <h5>Need help?</h5>
+          <h6>INST377homeCheck@gmail.com</h6>
+            
+        </div>
+        <div className="section_three">
+          <article className="contactMain">
+            <form>
+              <ul>
+                <li>
+                  <input
+                    type="text"
+                    id="name"
+                    name="user_name"
+                    placeholder="Name"
+                    maxLength="140"
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    id="email"
+                    name="user_email"
+                    placeholder="Email"
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    id="message"
+                    name="user_message"
+                    placeholder="Message"
+                  />
+                </li>
+              </ul>
+            </form>
+            <button type="submit">Send</button>
+          </article>
+        </div>
       </div>
     </div>
   </div>
--- public/components/DocumentationContent.js
+++ public/components/DocumentationContent.js
@@ -4,11 +4,11 @@ const DocumentationContent = () => (
   <div className="container">
     <div className="wrapper">
       <h1>How to Use Our Application</h1>
-      <div className = "content">      
+      <div className = "description">      
       </div>
       <h1>Want to Learn More Information?</h1>
-      <div className = "content">
-        <p>If you wish to learn more or work on this project, check out our <a href="https://github.com/danstruble/INST377_HomeCheck/blob/master/README.md">GitHub</a> project documentation! You can also send us any questions or inquiry you may have on our <i>Contact Us</i> page. </p>
+      <div className = "description">
+        <p>If you wish to learn more or work on this project, check out our <a href="https://github.com/danstruble/INST377_HomeCheck/blob/master/README.md">GitHub</a> project documentation! You can also send us any questions or inquiries you may have on our <i>Contact Us</i> page. </p>
       </div>
     </div>
   </div>
