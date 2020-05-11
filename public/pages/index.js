e8f84aeb07aa3f7c5ef840cdebffe09c48abbcd5
xiuweili1999 <60372954+xiuweili1999@users.noreply.github.com>
2020-05-11 02:29:51 -0400

    changes

--- public/pages/index.js
+++ public/pages/index.js
@@ -1,56 +1,83 @@
-import React from 'react';
-import Layout from '../components/Layout';
-import Search from '../components/Search';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet-universal';
-import Typewriter from 'typewriter-effect';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import Layout from "../components/Layout";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet-universal";
+import Typewriter from "typewriter-effect";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 
+const pointerIcon = new Icon({
+  iconUrl: '../images/home.svg',
+  iconSize: [25, 25]
+})
 
 
 export default class IndexPage extends React.Component {
-
   constructor() {
     super();
     this.state = {
-      violations: []
-    }
+      violations: [],
+    };
   }
 
   componentDidMount() {
-    this.getData()
+    this.getData();
   }
 
   getData() {
-    fetch('/api')
+    fetch("/api")
       .then((data) => data.json())
       .then((data) => {
         this.setState({
-          violations: data.data
-        })
-      })
+          violations: data.data,
+        });
+      });
   }
 
   renderLeaflet() {
     const violations = this.state.violations;
-    console.log(violations)
+    console.log(violations);
     return (
       <Map center={[38.7849, -76.8721]} zoom={9.5}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         {violations.map((violations, idx) => (
-          <Marker position={[parseFloat(violations.latLng.lat), parseFloat(violations.latLng.lng)]} key={idx}>
-            <Popup>{violations.address.street_number} {violations.address.street_name} {violations.address.street_suffix} {violations.address.city} {violations.address.state} {violations.address.zip} <br /><b>Inspection ID: </b>{violations.violations[0].inspectionID}<br /><b>Violation ID: </b>{violations.violations[0].violationID}<br /><b>Code: </b>{violations.violations[0].code}<br /><b>Description: </b>{violations.violations[0].desc}<br /><b>Count: </b>{violations.count}</Popup>
+          <Marker
+            position={[
+              parseFloat(violations.latLng.lat),
+              parseFloat(violations.latLng.lng),
+            ]}
+            key={idx}
+            icon={pointerIcon}
+          >
+            <Popup>
+              {violations.address.street_number}{" "}
+              {violations.address.street_name}{" "}
+              {violations.address.street_suffix} {violations.address.city}{" "}
+              {violations.address.state} {violations.address.zip} <br />
+              <b>Inspection ID: </b>
+              {violations.violations[0].inspectionID}
+              <br />
+              <b>Violation ID: </b>
+              {violations.violations[0].violationID}
+              <br />
+              <b>Code: </b>
+              {violations.violations[0].code}
+              <br />
+              <b>Description: </b>
+              {violations.violations[0].desc}
+              <br />
+              <b>Count: </b>
+              {violations.count}
+            </Popup>
           </Marker>
         ))}
       </Map>
-    )
+    );
   }
   render() {
-
     return (
       <Layout>
         <div className="container">
@@ -63,22 +90,21 @@ export default class IndexPage extends React.Component {
             }}
             />
             */}
-            <div className="map">
-              {this.renderLeaflet()}
-            </div>
-            <div className='search-box'>
-              <input className='search-txt' type='text' name="" placeholder='Type to search' />
-              <a className='search-btn' href="#">
+            <div className="map">{this.renderLeaflet()}</div>
+            <div className="search-box">
+              <input
+                className="search-txt"
+                type="text"
+                name=""
+                placeholder="Type to search"
+              />
+              <a className="search-btn" href="#">
                 <FontAwesomeIcon icon={faSearch} />
               </a>
             </div>
           </div>
-
         </div>
-
       </Layout>
     );
-
   }
-
 }
