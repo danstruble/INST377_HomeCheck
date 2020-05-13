156e597b2490ef00bfb9784b32fc1173efdbb260
xiuweili1999 <60372954+xiuweili1999@users.noreply.github.com>
2020-05-12 20:56:14 -0400

    updated documentation

--- public/components/DocumentationContent.js
+++ public/components/DocumentationContent.js
@@ -41,14 +41,61 @@ const DocumentationContent = () => (
         </ol>
       </div>
       <h1>Developer Notes</h1>
-      <div className="descriptions"></div>
+      <div className="description">
+        <h2>PUT</h2>
+        <div className = 'api_text'>
+          /api/search
+        </div>
+        <p>
+          This is a PUT request. This is called when a user first clicks the
+          search button on the Home Page, and sends a JSON object which contains
+          a keypair of "search", and the value being a string of the search term
+          entered by the user. This will then set the session search criteria to
+          be used in the results API. This API call also ensures that if the
+          violation data has not been fetched from the PG County Interface, that
+          it retrieves it. This specific call is asynchronous, which will
+          prevent search results from returning without the dataset being
+          populated on the server. After this call is completed, it will return
+          a 200 code, showing it has successfully completed.
+        </p>
+        <br></br>
+
+        <h2>GET</h2>
+        <div className = 'api_text'>
+        /api/search/results
+        </div>
+        <p>
+          This is a GET request. This is called after the /api/search call is
+          performed after the user has searched for a location. Being a GET
+          request, no data is required to be sent. This will take the session
+          search which was set by the PUT request called previously, and search
+          for a matching address in the violation set. If a match is found, it
+          will return the geocoded coordinates of the location to display on the
+          map, and the information about the location, containing violation
+          data. This will then be displayed on the leaflet map.
+        </p>
+        <br></br>
+
+        <h2>POST</h2>
+        <div className = 'api_text'>
+        /api/form/submit
+        </div>
+        <p>
+          This is a POST endpoint. At the minimum, a request with the body
+          content of a JSON object consisting of a "name" field, "email" field,
+          and "message" field shall be sent. This handles our Contact Us form,
+          which enters what the user has submitted on the form into a local
+          database currently hosted in the application memory. After the form
+          has been successfully submitted, it will return a success message.
+        </p>
+      </div>
       <h1>Want to Learn More Information?</h1>
       <div className="description">
         <p>
           If you wish to learn more or work on this project, check out our{" "}
           <a href="https://github.com/danstruble/INST377_HomeCheck/blob/master/README.md">
             GitHub
-          </a>{" "}
+          </a>
           project documentation! You can also send us any questions or inquiries
           you may have on our <i>Contact Us</i> page.{" "}
         </p>
